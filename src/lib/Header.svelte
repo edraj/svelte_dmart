c74e7e17993c0e631174fc493b57dc51132e7eb1
splimter <11891837+splimter@users.noreply.github.com>
2024-04-28 12:10:50 +0100

    impl search feat

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,6 +2,53 @@
   import {Navbar, NavbarBrand, Nav, NavLink, NavItem} from "sveltestrap";
   import { _ } from "@/i18n";
   import { user, signout } from "@/stores/user";
+  import markdownFiles from "@/md_indexer.json";
+  import {goto} from "@roxi/routify";
+
+
+
+  let term = '';
+  let suggestions = [];
+  let delayTimer;
+  async function handleInputChange(event: any) {
+      console.log('handleInputChange', event.target.value);
+      clearTimeout(delayTimer);
+      delayTimer = setTimeout(async function() {
+          console.log('searching for', event.target.value);
+          suggestions = [];
+          term = event.target.value;
+          if (term===""){
+              return;
+          }
+          const _term = term.toLowerCase();
+          for (const file of markdownFiles) {
+              try {
+                  const _result = file.content.toLowerCase();
+                  if (_result.includes(_term)) {
+                      suggestions = [
+                          ...suggestions,
+                          {
+                              path: file.path,
+                              title: file.filename.replace('.md', '').replace(/-/g, ' ').replace("index", "Home"),
+                              description: "..."+_result.substring(
+                                  _result.indexOf(_term)-50, _result.indexOf(_term)+50
+                              ).replace(_term.toLowerCase(), "<b>"+_term+"</b>")+"..."
+                          }
+                      ];
+                  }
+              } catch (e) {
+              }
+          }
+      }, 1000);
+  }
+
+  // Function to handle suggestion click
+  function handleSuggestionClick(suggestion: any) {
+      console.log(`going to ${suggestion.path}`);
+      $goto(`${suggestion.path.replace('.md', '')}`);
+      suggestions = [];
+  }
+
 </script>
 
 <Navbar color="light" light expand="md" class="px-2 py-0"
@@ -9,8 +56,34 @@
   <NavbarBrand href="/" title="{$_('home')}">{$_('home')}</NavbarBrand>
   <Nav navbar class="d-flex justify-content-between" style="width: 100%">
     {#if $user && $user.signedin}
-      <NavItem>{$user.localized_displayname}</NavItem>
+      <NavItem class="d-flex align-items-center">{$user.localized_displayname}</NavItem>
     {/if}
+    <NavItem class="w-75 align-content-center">
+    <div class="container">
+      <div class="position-relative">
+        <input type="text" class="form-control" placeholder="Search..."
+               bind:value={term}
+               on:input={handleInputChange}
+               on:focusout={()=>{
+                  setTimeout(()=>{
+                      suggestions = [];
+                  }, 200);
+               }}
+               on:focusin={handleInputChange}>
+        {#if suggestions.length > 0}
+          <ul class="list-group suggestion-list">
+            {#each suggestions as suggestion}
+              <!-- svelte-ignore a11y-no-noninteractive-element-interactions a11y-click-events-have-key-events -->
+              <li class="search-item list-group-item" on:click={() => handleSuggestionClick(suggestion)}>
+                <h5>{suggestion.title}</h5>
+                {@html suggestion.description}
+              </li>
+            {/each}
+          </ul>
+        {/if}
+      </div>
+    </div>
+    </NavItem>
     {#if !$user || !$user.signedin}
       <NavLink href="/login" title="{$_('login')}">{$_('login')}</NavLink>
     {:else}
@@ -18,3 +91,18 @@
     {/if}
   </Nav>
 </Navbar>
+<style>
+    .search-item:hover {
+        background-color: #ccc;
+    }
+    .search-item {
+        cursor: pointer;
+    }
+    .suggestion-list {
+        position: absolute;
+        z-index: 2;
+        width: 100%;
+        background-color: white;
+        border: 1px solid #ccc;
+    }
+</style>
---
+++ src/lib/MarkdownRenderer.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import Tags from "./Tags.svelte";
+
+    export let doc = "";
+    export let config = {};
+    export let components = new Map([
+        ["Callout", Callout],
+        ["ListView", ListView],
+    ]);
+    import Markdoc from "@markdoc/markdoc";
+    import yaml from "js-yaml";
+    import Callout from "./Callout.svelte";
+    import ListView from "./ListView.svelte";
+
+    export function add_frontmatter(ast, config) {
+        const frontmatter = ast.attributes.frontmatter ? yaml.load(ast.attributes.frontmatter) : {};
+        const markdoc = Object.assign(config?.variables?.markdoc || {}, { frontmatter });
+        const variables = Object.assign(config?.variables || {}, { markdoc });
+        return Object.assign(config, { variables });
+    }
+
+    const ast = Markdoc.parse(doc);
+    const config_with_frontmatter = add_frontmatter(ast, config);
+    const content: any = Markdoc.transform(ast, config_with_frontmatter);
+    console.log({content})
+</script>
+
+<Tags children={content.children} {components}></Tags>
